a8a9086d06fe69d8e336a635849e6609fc641546
Matt Webb <mattweb@WHM5003601.local>
2016-07-28 08:13:50 +0100

    added logger service and schedule

---
+++ modules/logger.service.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const log4js = require( 'log4js' );
+const config = require( '../config/app.json' );
+
+log4js.configure( config.logger );
+
+const logger = log4js.getLogger( 'app' );
+
+module.exports = logger;
--- modules/schedule.service.js
+++ modules/schedule.service.js
@@ -1,9 +1,44 @@
-/*
+'use strict';
 
-runNow
+const schedule = require( 'node-schedule' );
+const log = require( './logger.service' );
 
-scheduleNextRun
+class ScheduleService {
 
-scheduleConfig
+    constructor ( config ) {
+        this.cron = config.cron;
+        this.job = null;
+    }
 
-*/
+    start ( callback ) {
+        if ( this.cron ) {
+
+            log.trace( 'Starting Schedule Service', this.cron );
+
+            this.job = schedule.scheduleJob( this.cron, function() {
+                callback();
+            } );
+
+        } else {
+
+            throw new Error( 'No config cron settings provided!' );
+
+        }
+    }
+
+    stop ( callback ) {
+        if ( this.job ) {
+
+            log.trace( 'Stopping Schedule Service', this.job );
+
+            this.job.cancel();
+
+            if ( callback ) {
+                callback();
+            }
+
+        }
+    }
+}
+
+module.exports = ReportSchedule;
