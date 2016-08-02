9f14377818db1c3beaf4637b4d3e1d80dd8839c6
Matt Webb <matt.d.webb@icloud.com>
2016-08-02 08:09:23 +0100

    changed app config file to json

--- modules/player.service.js
+++ modules/player.service.js
@@ -3,6 +3,7 @@
 const http = require('http');
 const fs = require('fs');
 const AdmZip = require('adm-zip');
+const log = require('./logger.service');
 
 class FidePlayerService {
 
@@ -17,22 +18,27 @@ class FidePlayerService {
      * @param {string} fileName
      */
     download(fileName) {
+        log.trace('Initialised Download', fileName);
         const that = this;
         return new Promise(function(fulfill, reject) {
             try {
                 let file = fs.createWriteStream(that.fide.folder + '/' + fileName);
+                log.trace('Executing HTTP GET', that.fide.url);
                 http.get(that.fide.url, function(response) {
                     response.pipe(file);
+                    log.trace('Successful response.');
                     file.on('finish', function() {
                         file.close();
                         fulfill(fileName);
                     });
                 }).on('error', function(error) {
                     fs.unlink(that.fide.folder);
+                    log.error('Error on HTTP GET', error);
                     reject(new Error(error));
                 });
             } catch (error) {
-                reject(new Error(err));
+                log.error('Unexpected Error', error);
+                reject(new Error(error));
             }
         });
     }
@@ -43,6 +49,7 @@ class FidePlayerService {
      * @param {string} file
      */
     extract(file) {
+        log.trace('Extracting XML file from Zip', file);
         const that = this;
         return new Promise(function(fulfill, reject) {
 
@@ -57,11 +64,14 @@ class FidePlayerService {
                 if (zipEntry.entryName === that.fide.xmlFile) {
                     try {
                         zip.extractEntryTo(that.fide.xmlFile, that.fide.folder, false, true);
+                        log.trace('Extract complete.');
                         fulfill('Extract complete.');
                     } catch (error) {
+                        log.trace('Error', error);
                         reject(new Error(error));
                     }
                 } else {
+                    log.trace('Error Unable to match file');
                     reject(new Error('Unable to match file'));
                 }
             });
